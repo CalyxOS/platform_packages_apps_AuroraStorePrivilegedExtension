28e27a0f2c2f41ae26f760b5549d8fcbde72d9f8
Oliver Scott <olivercscott@gmail.com>
2021-03-02 16:51:24 -0500

    Whitelist privileged extension access for Aurora Store
    Change-Id: I1e55340fb04f0be89bc9e4f0896f28630e42fdbe

--- app/src/main/java/com/aurora/services/ClientWhitelist.java
+++ app/src/main/java/com/aurora/services/ClientWhitelist.java
@@ -34,8 +34,8 @@ import java.util.HashSet;
 public class ClientWhitelist {
 
     public static HashSet<Pair<String, String>> whitelist = new HashSet<>(Arrays.asList(
-            // certificate SHA-256 of https//f-droid.org/F-Droid.apk
-            new Pair<>("org.fdroid.fdroid", "43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab")
+            // certificate SHA-256 of Aurora Store apk
+            new Pair<>("com.aurora.store.beta", "4c626157ad02bda3401a7263555f68a79663fc3e13a4d4369a12570941aa280f")
     ));
 
 }
